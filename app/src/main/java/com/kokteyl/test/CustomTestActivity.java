bce056a4a21efc87e684812bb8ac95c155e7ca2e
CEM ÇATIKKAŞ <cem.catikkas@kokteyl.com>
2016-10-14 11:29:40 +0300

    AMR sdk 1.2.0

---
+++ app/src/main/java/com/kokteyl/test/CustomTestActivity.java
@@ -0,0 +1,24 @@
+package com.kokteyl.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class CustomTestActivity extends Activity {
+
+    private String TAG = "MILLEN_";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_test);
+
+
+    }
+
+
+
+
+
+
+}
